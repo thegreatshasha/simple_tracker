c7c2f26a2006657b380a55dfc74646f1774909a5
monisha <thegreatshasha@gmail.com>
2018-04-01 11:24:32 +0530

    add *

---
+++ visualize.py
@@ -0,0 +1,43 @@
+import numpy as np
+import glob
+from skimage.io import imread
+import matplotlib.pyplot as plt
+import random
+from tqdm import tqdm
+
+def query(track_data, t):
+	return track_data[np.isin(track_data[:,0], range(t+1-10, t+1))][:,[1,2]]
+
+track_fls = glob.glob('tracks/*.csv')
+track_fls_random = random.sample(track_fls, 20)
+img_fls = sorted(glob.glob('test_images/*'))
+det_fls = sorted(glob.glob('detections_csv/*'))
+detections = np.load('data.npy')
+print()
+
+
+colors = [(np.random.random(size=3) * 256) for i in range(len(track_fls_random))]
+
+
+for t, img_fl in tqdm(enumerate(img_fls)):
+	img = imread(img_fl)
+	det = np.loadtxt(det_fls[t], delimiter=',')
+
+	plt.figure()
+	plt.imshow(img)
+	plt.scatter(det[:,0], det[:,1], s=8, c='black', marker='x')
+	
+	for j, t_fl in enumerate(track_fls_random):
+		track_data = np.loadtxt(t_fl, delimiter=',')
+		xy = query(track_data, t)
+
+		#print(xy)
+
+		
+		plt.scatter(xy[:,0], xy[:,1], s=2)
+	
+	plt.savefig('tracks_outputs/%d'%t,dpi=100)
+	plt.close()
+
+
+print(track_fls)
