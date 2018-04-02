36a6adbc1f6acca6a19e0f3c099f9e6024053ebc
Shashwat Kumar <thegreatshasha@gmail.com>
2018-04-02 13:48:51 +0530

    adding funny cases

--- visualize.py
+++ visualize.py
@@ -9,7 +9,7 @@ def query(track_data, t):
 	return track_data[np.isin(track_data[:,0], range(t+1-10, t+1))][:,[1,2]]
 
 track_fls = glob.glob('tracks/*.csv')
-track_fls_random = random.sample(track_fls, 20)
+track_fls_random = random.sample(track_fls, 30)
 img_fls = sorted(glob.glob('test_images/*'))
 det_fls = sorted(glob.glob('detections_csv/*'))
 detections = np.load('data.npy')
@@ -23,9 +23,9 @@ for t, img_fl in tqdm(enumerate(img_fls)):
 	img = imread(img_fl)
 	det = np.loadtxt(det_fls[t], delimiter=',')
 
-	plt.figure()
+	plt.figure(figsize=(10,10))
 	plt.imshow(img)
-	plt.scatter(det[:,0], det[:,1], s=8, c='black', marker='x')
+	plt.scatter(det[:,0], det[:,1], s=16, c='black', marker='x')
 	
 	for j, t_fl in enumerate(track_fls_random):
 		track_data = np.loadtxt(t_fl, delimiter=',')
@@ -34,9 +34,9 @@ for t, img_fl in tqdm(enumerate(img_fls)):
 		#print(xy)
 
 		
-		plt.scatter(xy[:,0], xy[:,1], s=2)
+		plt.scatter(xy[:,0], xy[:,1], s=5)
 	
-	plt.savefig('tracks_outputs/%d'%t,dpi=100)
+	plt.savefig('tracks_outputs/%d'%t)
 	plt.close()
 
 
